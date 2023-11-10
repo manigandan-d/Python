41f036514efe661a7631777bca387999728a432b
manigandan-d <manigandan220916@gmail.com>
2023-11-10 19:00:17 +0530

    len function modified

--- Built-in Functions/len.py
+++ Built-in Functions/len.py
@@ -1,3 +1,18 @@
 name = "MANIGANDAN D"
-
 print(len(name))
+
+data = [10, 2.5, 'A', "Python"]
+print(len(data))
+
+data = (10, 2.5, 'A', "Python")
+print(len(data))
+
+details = {"Name": "Mani", "Age": 22, "Gender": 'M'}
+print(len(details))
+
+data = {10, 2.5, 'A', "Python", 2.5, 10}
+print(len(data))
+
+data = {10, 2.5, 'A', "Python", 2.5, 10}
+frozenData = frozenset(data)
+print(len(data))
