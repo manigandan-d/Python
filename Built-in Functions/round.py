7a3978e5d661c86d7d863babf12c28576a04b5f1
manigandan-d <manigandan220916@gmail.com>
2023-11-11 15:54:13 +0530

    updated

--- Built-in Functions/round.py
+++ Built-in Functions/round.py
@@ -1,5 +1,31 @@
+print(round(5))
+print(round(5, 2))
+
 print(round(5.2))
 print(round(5.7))
+print(round(5.5))
 
-print(round(5.234, 2))
+print(round(2.3))
+print(round(2.8))
+print(round(2.5))
+
+print(round(5.673, 2))
 print(round(5.678, 2))
+print(round(5.675, 2))
+
+print(round(5.623, 2))
+print(round(5.628, 2))
+print(round(5.625, 2))
+
+print(round(678, 2))
+print(round(678, 9))
+print(round(678, 0))
+
+print(round(678, -1))
+print(round(674, -1))
+print(round(675, -1))
+print(round(665, -1))
+
+print(round(678, -2))
+print(round(678, -3))
+print(round(678, -4))
