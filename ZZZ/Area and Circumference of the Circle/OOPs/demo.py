c6cfa5ef660e3b4cbb789757eb99e246098e7b7c
manigandan-d <manigandan220916@gmail.com>
2023-12-28 12:48:12 +0530

    updated

--- ZZZ/Area and Circumference of the Circle/OOPs/demo.py
+++ ZZZ/Area and Circumference of the Circle/OOPs/demo.py
@@ -1,14 +1,16 @@
 import math
 
 class Circle:
-	def __init__(self, r):
+	pi = math.pi
+	
+	def __init__(self, r=5):
 		self.r = r
 		
 	def area(self):
-		print(math.pi * self.r * self.r)
+		print(Circle.pi * self.r * self.r)
 	
 	def circumference(self):
-		print(2 * math.pi * self.r)
+		print(2 * Circle.pi * self.r)
 	
 
 
@@ -16,4 +18,11 @@ c1 = Circle(5)
 
 c1.area()
 c1.circumference()
+
+"""
+c2 = Circle(10)
+
+c2.area()
+c2.circumference()
+"""
 
