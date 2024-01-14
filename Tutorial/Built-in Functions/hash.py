ee242d95041e9501d85fc239f879fe471115b911
manigandan-d <manigandan220916@gmail.com>
2024-01-14 16:49:05 +0530

    hash function

---
+++ Tutorial/Built-in Functions/hash.py
@@ -0,0 +1,22 @@
+# hasoh(obj) -> obj should be immutable
+
+intData = 10
+print(hash(intData))
+
+floatData = 5.2
+print(hash(floatData))
+
+strData = "python"
+print(hash(strData))
+
+# listData = [1, 2, 3, 4, 5]
+# print(hash(listData))
+
+tupleData = (1, 2, 3, 4, 5)
+print(hash(tupleData))
+
+# dictData = {1: "abc", 2: "xyz"}
+# print(hash(dictData))
+
+# setData = {1, 2, 3}
+# print(hash(setData))
