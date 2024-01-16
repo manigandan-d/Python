067a0ffbcf9971db73a02ebd16729921a035add6
manigandan-d <manigandan220916@gmail.com>
2024-01-16 19:44:54 +0530

    wrap the given text

--- Exercises/Text Wrap/main.py
+++ Exercises/Text Wrap/main.py
@@ -5,12 +5,10 @@
 import textwrap
 
 
-def wrap(string, max_width):
-    wrapper = textwrap.TextWrapper(width=max_width)
-    wordList = wrapper.fill(text=string)
+def wrap(s, mw):
+    res = textwrap.fill(s, mw)
 
-    return wordList
-
+    return res
 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
