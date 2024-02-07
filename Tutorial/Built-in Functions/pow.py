3ea17b413b7dfb529f114c58ce7da8353ce55c65
manigandan-d <manigandan220916@gmail.com>
2024-02-07 11:09:46 +0530

    pow function

--- Tutorial/Built-in Functions/pow.py
+++ Tutorial/Built-in Functions/pow.py
@@ -1,4 +1,19 @@
-a = 5
-b = 2
+# without mod value 
+print(pow(2, 3))
 
-print(pow(a, b))
+# with mod value 
+print(pow(2, 3, 5))
+
+# cases in pow function
+
+# pos & pos -> int (pos)
+print(pow(2, 3))
+
+# pos & neg -> float (pos)
+print(pow(2, -3))
+
+# neg & pos -> int (neg)
+print(pow(-2, 3))
+
+# neg & neg -> float (neg)
+print(pow(-2, -3))
