a2839d9852d3734aad607b994c1df3ab6d4d26a8
manigandan-d <manigandan220916@gmail.com>
2024-02-07 16:32:53 +0530

    Armstrong Number

--- Exercises/Armstrong Number/main.py
+++ Exercises/Armstrong Number/main.py
@@ -1,3 +1,25 @@
+# 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407, 1634, 8208, 9474
+
 num = int(input("Enter the number: "))
 
-orgNum = num
+temp = num
+
+cnt = 0
+sum = 0
+
+while temp != 0:
+    cnt += 1
+    temp //= 10
+
+temp = num
+
+for i in range(cnt):
+    digit = temp % 10
+    sum += (digit ** cnt)
+    temp //= 10
+    
+if sum == num:
+    print(f"{num} is an Armstrong number")
+    
+else: 
+    print(f"{num} is not an Armstrong number")
