e7fcc0323010e03ace50fed444ec9454e77f8bf8
manigandan-d <manigandan220916@gmail.com>
2024-02-09 14:56:11 +0530

    guess the number from 1 to 10

---
+++ Exercises/Guess the number/main.py
@@ -0,0 +1,16 @@
+import random
+
+num = random.randint(1, 10)
+
+guess = int(input("Enter a number between 1 and 10: "))
+
+while guess != num:
+    if guess < num:
+        print("Your guess is too low")
+        
+    elif guess > num: 
+        print("Your guess is too high")
+        
+    guess = int(input("Guess again: "))
+    
+print("You guessed it right!")
