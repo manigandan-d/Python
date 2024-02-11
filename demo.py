cb7ec2da416bdf51cef858e60ecd01547cd75a4e
manigandan-d <manigandan220916@gmail.com>
2024-02-11 11:41:14 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,1 +1,16 @@
-print("Hey MD")
+import random
+
+num = random.randint(1, 10)
+
+guess = int(input("Enter a number between 1 and 10: "))
+
+while guess != num:
+    if guess < num:
+        print("Your guess is too low")
+        
+    elif guess > num: 
+        print("Your guess is too high")
+        
+    guess = int(input("Guess again: "))
+    
+print("You guessed it!")
