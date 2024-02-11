d96047e6f041dcf16698a2954ddd6fad03fc9262
manigandan-d <manigandan220916@gmail.com>
2024-02-11 15:43:31 +0530

    duck typing

--- Tutorial/OOPs/Polymorphism/duck_typing.py
+++ Tutorial/OOPs/Polymorphism/duck_typing.py
@@ -1,23 +1,24 @@
+# duck test -> if it looks like a duck, swims like a duck and quacks like a duck then it probablu is a duck
+
 class VSCode:
-    def execute():
+    def execute(self):
         print("general")
         print("extensions")
         print("compile")
         print("run")
 
 class PyCharm:
-    def execute():
+    def execute(self):
         print("specific")
         print("features")
         print("compile")
         print("run")
 
-class Laptop:
+class Code:
     def __init__(self, ide):
         ide.execute()
 
-c_ide = VSCode()
 cpp_ide = VSCode()
 python_ide = PyCharm()
 
-lap1 = Laptop(c_ide)
+palindrome = Code(python_ide)
