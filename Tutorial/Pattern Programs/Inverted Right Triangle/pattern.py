de4231d1f5d4ca7c9b8f1abcd4394396ade59d7f
manigandan-d <manigandan220916@gmail.com>
2024-02-25 17:36:36 +0530

    inverted right triangle

--- Tutorial/Pattern Programs/Inverted Right Triangle/pattern.py
+++ Tutorial/Pattern Programs/Inverted Right Triangle/pattern.py
@@ -7,6 +7,6 @@
 n=5
 
 for i in range(n):
-    for j in range(5, i, -1):
+    for j in range(n, i, -1):
         print("*", end="")
     print()
---
+++ demo.py
@@ -0,0 +1,6 @@
+n=5
+
+for i in range(n):
+    for j in range(5, i, -1):
+        print("*", end="")
+    print()
