3bf21f4ed38d492d05819f63caab0473b28bc8c3
manigandan-d <manigandan220916@gmail.com>
2024-02-25 20:19:37 +0530

    right triangle

--- Tutorial/Pattern Programs/Right Triangle/pattern.py
+++ Tutorial/Pattern Programs/Right Triangle/pattern.py
@@ -1,12 +1,25 @@
-# *
-# **
-# ***
-# ****
-# *****
+# * 
+# * *
+# * * *
+# * * * *
+# * * * * *
 
 n=5
 
-for i in range(n):
-    for j in range(i+1):
-        print("*", end="")
+for row in range(n):
+    for col in range(row+1):
+        print("*", end=" ")
+    print()
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5 
+
+n=5
+
+for row in range(1, n+1):
+    for col in range(1, row+1):
+        print(col, end=" ")
     print()
