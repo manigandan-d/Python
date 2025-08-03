1e8ffe215cf61131f567533d808e9af15cbba2c8
manigandan-d <manigandan220916@gmail.com>
2025-08-03 11:55:15 +0530

    input function

---
+++ input.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+print("Hello " + name)
+
+age = int(input("Enter your age: "))
+print(f"You are {age} years old.")
+
+x, y = input("Enter two numbers separated by space: ").split()
+print(x, y)
