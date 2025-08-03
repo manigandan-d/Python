c48998e115d57a4256b19bee155024ab9c683f1a
manigandan-d <manigandan220916@gmail.com>
2025-08-03 12:27:50 +0530

    f-strings (Formatted String Literals)

---
+++ f_strings.py
@@ -0,0 +1,15 @@
+name = "Mike"
+age = 25
+print(f"My name is {name} and I am {age} years old.")
+
+x = 10
+y = 20
+print(f"{x} + {y} = {x + y}")
+
+pi = 3.1415926535
+print(f"Value of pi up to 2 decimal places: {pi:.2f}")
+print(f"Value of pi up to 3 decimal places: {pi:.3f}")
+
+print(f"{"Python":<10} is fun!")  
+print(f"{"Python":^10} is fun!")   
+print(f"{"Python":>10} is fun!")   
