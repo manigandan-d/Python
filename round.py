88bd7bb8b3edb82fb4eaaf78af56b184db6cab63
manigandan-d <manigandan220916@gmail.com>
2025-08-03 12:23:38 +0530

    round function

---
+++ round.py
@@ -0,0 +1,12 @@
+print(round(4.7))
+print(round(4.3))
+
+print(round(3.14159, 2))
+print(round(3.14159, 3))
+
+print(round(1234, -1))
+print(round(1234, -2))
+
+# Python uses Banker's Rounding 
+print(round(2.5))
+print(round(3.5)) 
