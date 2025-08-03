558cffe4caa5c019283548e9f7ba565d6ba42d97
manigandan-d <manigandan220916@gmail.com>
2025-08-03 12:00:22 +0530

    type casting

---
+++ type_casting.py
@@ -0,0 +1,14 @@
+# Implicit type conversion 
+
+a = 10       
+b = 5.5     
+
+c = a + b   
+print(c)   
+print(type(c))
+
+# Explicit type conversion 
+
+num = input("Enter your favourite number: ")
+num = int(num)
+print(f"Your favourite number is {num}.")
