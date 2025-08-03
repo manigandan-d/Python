252642f16510e07a7ffa82751e400b4c177e1881
manigandan-d <manigandan220916@gmail.com>
2025-08-03 11:40:57 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,100 @@
+# Arithmetic Operators 
+
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+
+print()
+
+# Assignment Operators 
+
+x = 10
+
+x += 5
+print(x)
+
+x -= 3
+print(x)
+
+x *= 2
+print(x)
+
+x /= 4
+print(x)
+
+x //= 2
+print(x)
+
+x %= 2
+print(x)
+
+x **= 3
+print(x)
+
+print()
+
+# Relational Operators 
+
+m = 10
+n = 20
+
+print(m == n)
+print(m != n)
+print(m > n)  
+print(m < n) 
+print(m >= n)   
+print(m <= n)   
+
+print()
+
+# Logical Operators 
+
+z = 7
+
+print(z > 5 and z < 10)  
+print(z < 5 or z > 10)  
+print(not(z > 5))     
+
+print()
+
+# Bitwise Operators 
+
+p = 10 
+q = 4 
+
+print(p & q)   
+print(p | q)  
+print(p ^ q)  
+print(~p)   
+print(p << 1) 
+print(p >> 1)  
+
+print()
+
+# Membership Operators 
+
+text = "hello"
+
+print("h" in text)       
+print("z" not in text)  
+
+print()
+
+# Identity Operators 
+
+x = [1, 2, 3]
+y = x
+z = [1, 2, 3]
+
+print(x is y)      
+print(x == y)  
+
+print(x == z)
+print(x is z)
