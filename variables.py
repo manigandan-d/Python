7ce1608a2853aa2f6fbabc345022e6d6e8201c13
manigandan-d <manigandan220916@gmail.com>
2025-08-03 10:59:55 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,9 @@
+name = "Alex" 
+age = 30
+height = 5.9
+is_alien = False
+
+print("Name:", name, type(name))
+print("Age:", age, type(age))
+print("Height:", height, type(height))
+print("Is Alien:", is_alien, type(is_alien))
