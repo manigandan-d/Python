fba91d6afaf8bc49c5d1dc994acbcc47fc188b70
manigandan-d <manigandan220916@gmail.com>
2025-08-03 11:06:41 +0530

    data types

---
+++ data_types.py
@@ -0,0 +1,28 @@
+a = 5 
+b = 2.5 
+c = 3 + 4j 
+
+prog = "Python"
+
+is_valid = True
+
+fruits = ["apple", "banana", "cherry"]
+
+coords = (10.5, 20.6)
+
+person = {"name": "John", "age": 25}
+
+unique_values = {1, 2, 3, 4, 5}
+
+result = None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(prog))
+print(type(is_valid))
+print(type(fruits))
+print(type(coords))
+print(type(person))
+print(type(unique_values))
+print(type(result))
