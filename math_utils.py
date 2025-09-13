ee43db0c3491a7590b9354d23b5eb0f998fd6e8c
manigandan-d <manigandan220916@gmail.com>
2025-09-13 13:12:26 +0530

    __name__ usage

---
+++ math_utils.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+print("math_utils.py is being executed!")
+
+if __name__ == "__main__":
+    print("Testing functions...")
+    print(add(5, 3)) 
+    print(sub(5, 3)) 
